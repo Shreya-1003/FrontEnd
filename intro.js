0ab6572617faccfead21f4dac31f1040965d0f9a
Shreya Nandu Thorat <97801433+Shreya-1003@users.noreply.github.com>
2023-04-19 13:10:25 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+function printLength()
+{
+    var str=document.getElementById("name").Value;
+    var strlen=str.length;
+    const x=44;
+    document.getElementById("demo").innerHTML="your name is: "+Number(strlen+x);
+}
